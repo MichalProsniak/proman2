901ba727c20c23c807fd002a1792d56b9787e23d
Michal Prosniak <michalprosniak@gmail.com>
2022-03-30 11:29:31 +0200

    Add new cards bug fixes and page instant display

--- static/js/controller/cardsManager.js
+++ static/js/controller/cardsManager.js
@@ -27,7 +27,7 @@ export let cardsManager = {
     },
 };
 
-async function deleteButtonHandler(clickEvent) {
+export async function deleteButtonHandler(clickEvent) {
     const cardId = clickEvent.target.dataset.removeCardId
     let allCards = document.getElementsByClassName("card")
     for (let card of allCards) {
@@ -40,7 +40,7 @@ async function deleteButtonHandler(clickEvent) {
 }
 
 
-function changeColumnTitle(clickEvent) {
+export function changeColumnTitle(clickEvent) {
     const columnId = clickEvent.target.attributes['data-column-id'].nodeValue[0];
 
     let element = document.querySelector(`.board-column-title[data-column-id='${columnId}']`)
